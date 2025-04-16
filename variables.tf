c14f4d11be6e061cfdebd401fc0c1d6226351f2b
Chandrakala Bhogavarapu <146967411+BhogavarapuChandrakala@users.noreply.github.com>
2025-04-16 20:22:19 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "vm_admin_username" {
+  default = "adminuser"
+}
+
+variable "vm_admin_password" {
+  default = "P@ssword1234!"  # Use secrets management in real setup
+}
